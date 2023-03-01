680339eadbc38cd1e72ecb746d85a0cfdce5c8cb
mateobp04 <126584132+mateobp04@users.noreply.github.com>
2023-02-28 19:39:25 -0500

    Add files via upload

---
+++ CnPersonas/CnPersonas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPersonas
+{/// <summary>
+/// Mateo Becerra
+/// 28/02/2023
+/// Mayoria de edad
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Nombre");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Eres mayor de edad?");
+            Console.WriteLine("1 Sí");
+            Console.WriteLine("2 No");
+            string Mayor = Console.ReadLine();
+
+            if (Mayor == "1")
+            {
+
+                Formulario();
+
+            }
+            else if (Mayor == "2")
+            {
+                Console.WriteLine("Eres menor de edad");
+
+            }
+            Console.ReadKey();
+        }
+        private static void Formulario()
+        {
+
+          
+            Console.WriteLine("Apellido?");
+            Console.ReadLine();
+            Console.WriteLine("Edad");
+            Console.ReadLine();
+            Console.WriteLine("Correo electronico");
+            Console.ReadLine();
+            Console.WriteLine("adios");
+         
+        }
+       
+    }
+}
+
---
+++ cnTEMPERATURA/cnTEMPERATURA/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnTEMPERATURA
+{
+    internal class Program
+    {/// <summary>
+    /// Mateo Becerra
+    /// 28/02/2023
+    /// Cambio de temperatura
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+     
+            Console.WriteLine("Digitar 1 si lo necesita de celsius a farenheit");
+            Console.WriteLine("Digitar 2 si lo necesita de farenheit a celsius");
+            double Convertido = 0;
+            string TipoDeTemperatura = "";
+            TipoDeTemperatura=Console.ReadLine();
+  
+
+            if (TipoDeTemperatura == "1")
+            {
+                Console.WriteLine("Ingrese la temperatura en grados celsius");
+         
+                Console.WriteLine("celsius a farenheit es: " + ConversionTemperaturaCelsius(Convertido));
+              
+            }
+            else if (TipoDeTemperatura == "2")
+            {
+                Console.WriteLine("Ingrese la temperatura en grados farenheit");
+                Console.WriteLine("farenheit a celsius es: " + ConversionTemperaturaFarenheit(Convertido));
+
+            }
+
+
+            Console.ReadKey();
+        }
+      
+        private static double ConversionTemperaturaCelsius(double Convertido)
+        {
+            double  Temperatura = 0;
+           
+             Temperatura = double.Parse(Console.ReadLine());
+             Convertido = (Temperatura * (9 / 5) + 32);
+
+
+            return Convertido;
+        }
+    
+        private static double ConversionTemperaturaFarenheit(double Convertido)
+        {
+            double Temperatura = 0;
+            Temperatura = double.Parse(Console.ReadLine());
+            Convertido = (Temperatura - 32) * 5 / 9;
+
+
+            return Convertido;
+        
+        }
+
+    }
+}
